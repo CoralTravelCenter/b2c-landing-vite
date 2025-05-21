6fa6f5933067bcbd49773f0a9f9ca824622e7780
Mike <50363065+MikeGankin@users.noreply.github.com>
2025-05-21 19:05:29 +0300

    +++ Add KV

--- site/coral.ru/js/hero.js
+++
@@ -1,9 +0,0 @@
-import {hostReactAppReady} from "../../common/js/usefuls.js";
-
-(async () => {
-  await hostReactAppReady();
-
-  const a = document.querySelector('h1')
-  a.textContent = 'Hello World';
-  console.log(a)
-})()
